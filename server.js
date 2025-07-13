a18b3fbd64f63642e48b3e2fcb6cf3f5f0497022
likhithraj <likhithraj@xorstack.com>
2025-07-13 21:12:05 +0530

    [FIX] Cors issue in server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { initialize,processUserInput } from './app.js';
 import dotenv from 'dotenv';
 
@@ -10,8 +11,23 @@ process.env.NODE_ENV = 'server';
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// If you want to allow all origins
+// app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Server is running!' });
+});
 
 app.post('/q', async (req, res) => {
     const userInput = req.body.input;
